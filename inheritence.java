aac75da6d487e821671005794d1d90d07413cb35
AyushJPatel831 <144580256+AyushJPatel831@users.noreply.github.com>
2023-10-11 12:54:58 +0530

    Create inheritence.java

---
+++ inheritence.java
@@ -0,0 +1,35 @@
+class Animal{
+
+public void eat(){
+
+System.out.println("I can eat");
+
+}
+
+}
+
+class Dog extends Animal{
+
+public void eat(){ super.eat(); System.out.println("I eat dog food");
+}
+public void bark(){
+
+System.out.println("I can bark");
+
+}
+
+}
+
+public class Main{
+
+public static void main(String args[]){
+
+Dog labrador=new Dog();
+
+labrador.eat();
+
+labrador.bark();
+
+}
+
+}
